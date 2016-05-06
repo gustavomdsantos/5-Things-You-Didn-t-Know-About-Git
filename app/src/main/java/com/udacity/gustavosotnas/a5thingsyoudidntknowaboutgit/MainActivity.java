1c66adcc08b70e5df6f738eab37212c9f4d85317
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-05-05 21:24:03 -0300

    Criada uma Activity para cada item do menu principal (links feitos programaticamente ao invés de um "onClick" dentro do XML, para funcionar tanto em Portrait quando em Landscape)

--- app/src/main/java/com/udacity/gustavosotnas/a5thingsyoudidntknowaboutgit/MainActivity.java
+++ app/src/main/java/com/udacity/gustavosotnas/a5thingsyoudidntknowaboutgit/MainActivity.java
@@ -1,7 +1,10 @@
 package com.udacity.gustavosotnas.a5thingsyoudidntknowaboutgit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,78 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setButtonLinks();
+    }
+
+    private void setButtonLinks(){
+        Button thing1 = (Button) findViewById(R.id.thing1);
+        thing1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClick$NewCommitsActivity(view);
+            }
+        });
+
+        Button thing2 = (Button) findViewById(R.id.thing2);
+        thing2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClick$CommittingPersonActivity(view);
+            }
+        });
+
+        Button thing3 = (Button) findViewById(R.id.thing3);
+        thing3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClick$CommittingDateActivity(view);
+            }
+        });
+
+        Button thing4 = (Button) findViewById(R.id.thing4);
+        thing4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClick$MasteringGitTagsActivity(view);
+            }
+        });
+
+        Button thing5 = (Button) findViewById(R.id.thing5);
+        thing5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClick$BestPracticesActivity(view);
+            }
+        });
+    }
+
+    public void onClick$NewCommitsActivity(View view) {
+
+        Intent intent = new Intent(MainActivity.this, NewCommitsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick$CommittingPersonActivity(View view) {
+
+        Intent intent = new Intent(MainActivity.this, CommittingPersonActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick$CommittingDateActivity(View view) {
+
+        Intent intent = new Intent(MainActivity.this, CommittingDateActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick$MasteringGitTagsActivity(View view) {
+
+        Intent intent = new Intent(MainActivity.this, MasteringGitTagsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClick$BestPracticesActivity(View view) {
+
+        Intent intent = new Intent(MainActivity.this, BestPracticesActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/udacity/gustavosotnas/a5thingsyoudidntknowaboutgit/NewCommitsActivity.java
@@ -0,0 +1,16 @@
+package com.udacity.gustavosotnas.a5thingsyoudidntknowaboutgit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gustavosotnas on 05/05/16.
+ */
+public class NewCommitsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newcommits);
+        getSupportActionBar().setTitle("NewCommitsActivity");
+    }
+}
