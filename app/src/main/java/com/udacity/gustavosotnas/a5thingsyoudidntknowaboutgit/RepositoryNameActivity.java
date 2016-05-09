4e0e81e5e41fbc8afa649b7b7ac2c002b6c3fc9e
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-05-08 21:41:31 -0300

    Criado pequeno formulário para usuário preencher o nome do repositório Git alvo (ou usar o nome padrão "meu-repositorio-git" caso ele esteja com pressa).

--- app/src/main/java/com/udacity/gustavosotnas/a5thingsyoudidntknowaboutgit/RepositoryNameActivity.java
+++ app/src/main/java/com/udacity/gustavosotnas/a5thingsyoudidntknowaboutgit/RepositoryNameActivity.java
@@ -1,20 +1,65 @@
 package com.udacity.gustavosotnas.a5thingsyoudidntknowaboutgit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 /**
  * Created by gustavosotnas on 08/05/16.
  */
 public class RepositoryNameActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository_name);
+
+        focusKeyboard(R.id.repositoryEditText);
     }
 
+    /**
+     * Force keyboard to show focused in a specific EditText.
+     */
+    private void focusKeyboard(int R_id_editText)
+    {
+        EditText editText = (EditText) findViewById(R_id_editText);
+        editText.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+    }
+
+    /**
+     * When user clicks in "use_default_name_button", the app sets the hint
+     * text of the editText to the name of the Git repository in the static
+     * Repository variable in MainActivity.
+     * @param view not used here, it's necessary param to work with XML View.
+     */
+    public void onClick$defaultRepositoryName(View view)
+    {
+        MainActivity.getRepository().setName(getResources().getString(R.string.hint_activity_repository));
+    }
+
+    /**
+     * When user clicks in "save_button", the app sets the name of the Git
+     * repository entered in editText to the static Repository variable in
+     * MainActivity.
+     * @param view not used here, it's necessary param to work with XML View.
+     */
+    public void onClick$saveRepositoryName(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.repositoryEditText);
+        MainActivity.getRepository().setName(editText.getText().toString());
+
+        finish();
+    }
+
+    /**
+     * When user press "Back" button in this activity, the app will go to
+     * MainActivity.
+     */
     @Override
     public void onBackPressed()
     {
